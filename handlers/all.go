5e3ef59869879521ae77496d3657570e87b91d6f
Pan Wenbin <panwenbin@gmail.com>
2020-03-29 20:28:22 +0800

    add support for regexp rule

--- handlers/all.go
+++ handlers/all.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -9,7 +10,9 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -32,8 +35,32 @@ var hopHeaders = []string{
 	"Upgrade",
 }
 
+func isLocalIp(ipStr string) bool {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ipStrCheck := ipStr + "/"
+	for i := range interfaces {
+		addrs, err := interfaces[i].Addrs()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for j := range addrs {
+			addr := addrs[j]
+			if addr.Network() == "ip+net" {
+				addrStr := addr.String()
+				if strings.Compare(addrStr, ipStr) == 0 || strings.Index(addrStr, ipStrCheck) == 0 {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
 func All(writer http.ResponseWriter, request *http.Request) {
-	if request.Host == "127.0.0.1" {
+	if isLocalIp(request.Host) {
 		setting(writer, request)
 		return
 	}
@@ -55,28 +82,33 @@ func All(writer http.ResponseWriter, request *http.Request) {
 	if request.TLS != nil {
 		scheme = "https"
 	}
+	reqBody, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		writer.Write([]byte("read request body err"))
+		return
+	}
 	client := ghttpclient.NewClient().Url(scheme + "://" + request.Host + request.RequestURI).Headers(headers).NoRedirect(true)
 	switch request.Method {
 	case "GET":
 		client = client.Get()
 	case "POST":
-		client = client.Body(request.Body).Post()
+		client = client.Body(bytes.NewReader(reqBody)).Post()
 	default:
 		writer.Write([]byte(request.Method + " is not supported yet"))
 		return
 	}
-	res, err := client.Response()
+	response, err := client.Response()
 	if err != nil {
 		writer.Write([]byte("get response err: " + err.Error()))
 		return
 	}
 
-	contentType := res.Header.Get("Content-Type")
+	contentType := response.Header.Get("Content-Type")
 	switch {
 	case strings.Contains(contentType, "text"), strings.Contains(contentType, "javascript"), strings.Contains(contentType, "json"):
-		err = proxy(res, writer, request)
+		err = proxy(response, writer, request, reqBody)
 	default:
-		err = pass(res, writer, request)
+		err = pass(response, writer, request, reqBody)
 	}
 	if err != nil {
 		writer.Write([]byte("err occurs: " + err.Error()))
@@ -84,7 +116,15 @@ func All(writer http.ResponseWriter, request *http.Request) {
 }
 
 var outDomainMap = make(map[string]string, 0)
-var outDomainMapMutex = sync.RWMutex{}
+var outRules = make([]*outRule, 0)
+var outMutex = sync.RWMutex{}
+
+type outRule struct {
+	Regexp    string `json:"regexp"`
+	OutRegexp *regexp.Regexp
+	OutType   string `json:"out_type"`
+	OutServer string `json:"out_server"`
+}
 
 type out struct {
 	Method         string      `json:"method"`
@@ -95,16 +135,22 @@ type out struct {
 	ResponseBody   string      `json:"response_body"`
 }
 
-func outPrepare(request *http.Request, response *http.Response, body []byte) (string, string, []byte) {
-	outDomainMapMutex.RLock()
+func checkOut(request *http.Request) (string, string) {
+	uri := request.Host + request.RequestURI
+	outMutex.RLock()
+	for i := range outRules {
+		if outRules[i].OutRegexp.MatchString(uri) {
+			return outRules[i].OutType, outRules[i].OutServer
+		}
+	}
+
 	outSetting, ok := outDomainMap[request.Host]
 	if !ok {
 		outSetting, ok = outDomainMap["*"]
 	}
-	outType := ""
+	outType := "none"
 	outServer := ""
-	outBody := make([]byte, 0)
-	outDomainMapMutex.RUnlock()
+	outMutex.RUnlock()
 	if ok {
 		splits := strings.SplitN(outSetting, ":", 2)
 		outType = splits[0]
@@ -113,25 +159,28 @@ func outPrepare(request *http.Request, response *http.Response, body []byte) (st
 			remoteHost := strings.Split(request.RemoteAddr, ":")[0]
 			outServer = strings.Replace(outServer, "remote", remoteHost, 1)
 		}
-
-		reqBody, _ := ioutil.ReadAll(request.Body)
-		o := out{
-			Method:         request.Method,
-			Uri:            request.Host + request.RequestURI,
-			RequestHeader:  request.Header,
-			RequestBody:    string(reqBody),
-			ResponseHeader: response.Header,
-			ResponseBody:   string(body),
-		}
-		outBody, _ = json.Marshal(o)
 	}
 
-	return outType, outServer, outBody
+	return outType, outServer
 }
 
-func proxy(res *http.Response, writer http.ResponseWriter, request *http.Request) error {
+func prepareOut(request *http.Request, response *http.Response, reqBody []byte, resBody []byte) []byte {
+	o := out{
+		Method:         request.Method,
+		Uri:            request.Host + request.RequestURI,
+		RequestHeader:  request.Header,
+		RequestBody:    string(reqBody),
+		ResponseHeader: response.Header,
+		ResponseBody:   string(resBody),
+	}
+
+	outBody, _ := json.Marshal(o)
+	return outBody
+}
+
+func proxy(response *http.Response, writer http.ResponseWriter, request *http.Request, reqBody []byte) error {
 	h := writer.Header()
-	for k, vv := range res.Header {
+	for k, vv := range response.Header {
 		if k == "Content-Encoding" {
 			continue
 		}
@@ -147,44 +196,47 @@ func proxy(res *http.Response, writer http.ResponseWriter, request *http.Request
 			delete(h, hopHeader)
 		}
 	}
-	body, err := ghttpclient.ReadBodyClose(res)
+	resBody, err := ghttpclient.ReadBodyClose(response)
 	if err != nil {
 		return errors.New("read body err: " + err.Error())
 	}
-	outType, outServer, outBody := outPrepare(request, res, body)
+	outType, outServer  := checkOut(request)
 	switch outType {
 	case "chan":
-		body, err = ghttpclient.PostJson("http://"+outServer+"/chan", outBody, nil).ReadBodyClose()
+		outBody := prepareOut(request, response, reqBody, resBody)
+		resBody, err = ghttpclient.PostJson("http://"+outServer+"/chan", outBody, nil).ReadBodyClose()
 		if err != nil {
 			log.Println(err)
 		}
 	case "log":
+		outBody := prepareOut(request, response, reqBody, resBody)
 		go ghttpclient.PostJson("http://"+outServer+"/log", outBody, nil).ReadBodyClose()
 	}
-	h.Set("Content-Length", strconv.Itoa(len(body)))
-	writer.WriteHeader(res.StatusCode)
-	_, err = writer.Write(body)
+	h.Set("Content-Length", strconv.Itoa(len(resBody)))
+	writer.WriteHeader(response.StatusCode)
+	_, err = writer.Write(resBody)
 	return err
 }
 
-func pass(res *http.Response, writer http.ResponseWriter, request *http.Request) error {
-	outType, outServer, outBody := outPrepare(request, res, nil)
+func pass(response *http.Response, writer http.ResponseWriter, request *http.Request, reqBody []byte) error {
+	outType, outServer  := checkOut(request)
 	switch outType {
 	case "log":
+		outBody := prepareOut(request, response, reqBody, nil)
 		go ghttpclient.PostJson("http://"+outServer+"/log", outBody, nil).ReadBodyClose()
 	}
-	defer res.Body.Close()
+	defer response.Body.Close()
 	h := writer.Header()
-	for k, vv := range res.Header {
+	for k, vv := range response.Header {
 		for _, v := range vv {
 			h.Set(k, v)
 		}
 	}
-	writer.WriteHeader(res.StatusCode)
+	writer.WriteHeader(response.StatusCode)
 
 	buf := make([]byte, 32*1024)
 	for {
-		n, err := res.Body.Read(buf)
+		n, err := response.Body.Read(buf)
 		if err != nil && err != io.EOF && err != context.Canceled {
 			return errors.New("read body err: " + err.Error())
 		}
@@ -202,18 +254,67 @@ func pass(res *http.Response, writer http.ResponseWriter, request *http.Request)
 }
 
 func setting(writer http.ResponseWriter, request *http.Request) {
-	splits := strings.Split(request.RequestURI, "/")
-	if len(splits) == 3 {
-		domain := splits[1]
-		outType := splits[2]
-		outDomainMapMutex.Lock()
-		switch outType {
-		case "none":
-			delete(outDomainMap, domain)
-		default:
-			outDomainMap[domain] = outType
+	switch request.Method {
+	case "GET":
+		splits := strings.Split(request.RequestURI, "/")
+		if len(splits) == 3 {
+			domain := splits[1]
+			outType := splits[2]
+			outMutex.Lock()
+			switch outType {
+			case "none":
+				delete(outDomainMap, domain)
+			default:
+				outDomainMap[domain] = outType
+			}
+			outMutex.Unlock()
+			writer.Write([]byte("set " + domain + " : " + outType + "\n"))
+			return
+		} else {
+			outJson, err := json.Marshal(map[string]interface{}{
+				"out_rules": outRules,
+				"out_domains": outDomainMap,
+			})
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			writer.Write(outJson)
+			return
 		}
-		outDomainMapMutex.Unlock()
-		writer.Write([]byte("set " + domain + " : " + outType))
+	case "POST":
+		reqBody, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			log.Println(err)
+			writer.Write([]byte("read body err"))
+			return
+		}
+
+		outR := outRule{}
+		err = json.Unmarshal(reqBody, &outR)
+		if err != nil {
+			log.Println(err)
+			writer.Write([]byte("json unmarshal err"))
+			return
+		}
+
+		outMutex.Lock()
+		defer outMutex.Unlock()
+		outR.OutRegexp, err = regexp.Compile(outR.Regexp)
+		if err != nil {
+			log.Println(err)
+			writer.Write([]byte("regexp err"))
+			return
+		}
+		for i:= range outRules {
+			if outRules[i].Regexp == outR.Regexp {
+				outRules[i] = &outR
+				writer.Write([]byte("set " + outR.Regexp + " : " + outR.OutType + "\n"))
+				return
+			}
+		}
+		outRules = append(outRules, &outR)
+		writer.Write([]byte("set " + outR.Regexp + " : " + outR.OutType + "\n"))
+		return
 	}
 }
